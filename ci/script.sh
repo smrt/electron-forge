ce8ac9ef63ccdb58db10f4cfb9f9124352ed4dc1
Mark Lee <electronjs@lazymalevolence.com>
2017-05-21 19:18:29 -0700

    Try adding linux capabilities to Docker container

--- ci/script.sh
+++ ci/script.sh
@@ -2,7 +2,7 @@
 
 if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
     sudo docker build -f ci/Dockerfile . -t electron-forge-ci
-    sudo docker run -it electron-forge-ci ci/docker.sh $NODE_INSTALLER
+    sudo docker run --cap-add=SYS_ADMIN -it electron-forge-ci ci/docker.sh $NODE_INSTALLER
 else
     if [[ "$NODE_INSTALLER" = "yarn" ]]; then npm i -g yarn; fi
     npm run test-coverage -- --installer=$NODE_INSTALLER
